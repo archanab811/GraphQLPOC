9e7f47429fe84ec8e9327c481d9e28bd8027cc39
archanab811 <59409585+archanab811@users.noreply.github.com>
2023-04-30 09:12:19 +0530

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphQL
+//
+//  Created by Archana Mahesh Chigare on 24/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
